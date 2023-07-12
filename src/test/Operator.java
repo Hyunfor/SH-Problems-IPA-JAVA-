bc38d30e573ebbf6a36072dc3bcc904872a0d31e
Hyunfor <navel5148@gmail.com>
2023-07-12 20:05:27 +0900

    21년 2회차 67번

---
+++ src/test/Operator.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+		// 21년 2회차 67번
+		
+		int x=5, y=0, z=0;
+		
+		System.out.println(y);
+		System.out.println(z);
+		
+		y = x++;
+		z = --x;
+		
+		System.out.println(x + "," + y + "," + z);
+
+	}
+
+}
