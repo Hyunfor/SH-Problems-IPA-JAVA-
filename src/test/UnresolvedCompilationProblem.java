13010a5c8b600bdac480946380365c5af9a8fce1
Hyunfor <navel5148@gmail.com>
2023-07-13 18:33:51 +0900

    20년 4회차 67번

---
+++ src/test/UnresolvedCompilationProblem.java
@@ -0,0 +1,15 @@
+package test;
+
+public class UnresolvedCompilationProblem {
+
+	public static void main(String[] args) {
+		// 20년 4회차 67번
+		int x = 1, y = 6;
+		while(y--) {
+			x++;
+		}
+		System.out.println("x=" + x + "y=" + y);
+
+	}
+
+}
